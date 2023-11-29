5eaea1ea8ccc7ed722ecec3ae8ae5e3a1ed062ec
Kwangil Park <rhkdkdlf136@gmail.com>
2023-11-29 18:22:31 +0900

    11월 29일 마감

--- src/lab/chap06/Product_Test.java
+++ src/lab/chap06/Product_Test.java
@@ -19,28 +19,28 @@ public class Product_Test {
 		
 		//2.setter를 사용해서 객체의 필드의 값을 주입
 		prod1.setIdx(1);
-		prod1.setProName("헤어드라이기");
-		prod1.setQty(10);
-		prod1.setYear(2020);
-		prod1.setProColor("검정색");
+		prod1.setProName("화장품");
+		prod1.setQty(100);
+		prod1.setYear(2000);
+		prod1.setProColor("초록");
 		
 		prod2.setIdx(2);
-		prod2.setProName("냉장고");
-		prod2.setQty(15);
-		prod2.setYear(2021);
-		prod2.setProColor("하얀색");
+		prod2.setProName("사과");
+		prod2.setQty(500);
+		prod2.setYear(2023);
+		prod2.setProColor("빨강");
 		
 		prod3.setIdx(3);
-		prod3.setProName("TV");
-		prod3.setQty(5);
+		prod3.setProName("컴퓨터");
+		prod3.setQty(50);
 		prod3.setYear(2022);
-		prod3.setProColor("실버");
+		prod3.setProColor("검은색");
 		
 		prod4.setIdx(4);
-		prod4.setProName("조명기구");
-		prod4.setQty(20);
-		prod4.setYear(2023);
-		prod4.setProColor("녹색");
+		prod4.setProName("모니터");
+		prod4.setQty(70);
+		prod4.setYear(2020);
+		prod4.setProColor("흰색");
 		
 //		System.out.println( prod1 );
 //		System.out.println( prod2 );
@@ -57,12 +57,18 @@ public class Product_Test {
 		arr[3] = prod4;
 		
 		//4.배열의 값을 끄집어내서 Product의 변수'p'에 담고 출력(for문)
+//		for (int i = 0; i < arr.length; i++) {
+//			Product p = arr[i];
+//			if (arr[i]==null) continue; //null 감추기
+//			System.out.println(p);
+//		}
+		System.out.println("==========================================");
 		for (int i = 0; i < arr.length; i++) {
-			Product p = arr[i];
-			System.out.println(p);
+			if (arr[i] != null) {
+				Product p = arr[i];
+				System.out.println(p);
+			}
 		}
-		
-		
 		
 		
 		
